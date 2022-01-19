ef86f5a9ae8959367817f569cc2b9cc970d86727
Marek <84468808+marekvks@users.noreply.github.com>
2022-01-19 15:41:03 +0100

    Animations

--- AngryZombies/Assets/Scripts/CameraFollow.cs
+++ AngryZombies/Assets/Scripts/CameraFollow.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+
+    public Camera cam;
+    private float offset;
+
     // Start is called before the first frame update
     void Start()
     {
--- AngryZombies/Assets/Scripts/PlayerMovement.cs
+++ AngryZombies/Assets/Scripts/PlayerMovement.cs
@@ -20,6 +20,8 @@ public class PlayerMovement : MonoBehaviour
     private float horizontal = 0f;
     private float vertical = 0f;
 
+    string dir;
+
     void Update()
     {
         Rotate();
@@ -31,36 +33,129 @@ public class PlayerMovement : MonoBehaviour
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
 
+        Vector3 direction = horizontal * transform.right + vertical * transform.forward;
+
         controller.Move(new Vector3(horizontal * speed * Time.deltaTime, 0, vertical * speed * Time.deltaTime));
 
         switch(horizontal, vertical)
         {
-          // Up & Right
-            case (1, 1):
-                // animator.SetFloat("run", num);
-                break;
-            // Down & Right
-            case (1, -1):
-                break;
-            // Up & Left
-            case (-1, 1):
-                break;
-            // Down & Left
-            case (-1, -1):
-                break;
-            // Right
-            case (1, 0):
-                break;
-            // Left
-            case (-1, 0):
-                break;
-            // Down
-            case (0, -1):
-                break;
-            //  Up
+            //  Up - 1
             case (0, 1):
+                if (dir == "NW" || dir == "NE")
+                {
+                    animator.SetInteger("direction", 2);
+                }
+                else
+                {
+                    animator.SetInteger("direction", 1);
+                }
+                break;
+            // Down - 2
+            case (0, -1):
+                if (dir == "NW" || dir == "NE")
+                {
+                    animator.SetInteger("direction", 2);
+                }
+                else
+                {
+                    animator.SetInteger("direction", 1);
+                }
+                break;
+            // Right - 3
+            case (1, 0):
+                if (dir == "NE" || dir == "SE")
+                {
+                    animator.SetInteger("direction", 3);
+                }
+                else
+                {
+                    animator.SetInteger("direction", 4);
+                }
+                break;
+            // Left - 4
+            case (-1, 0):
+                if (dir == "NE" || dir == "SE")
+                {
+                    animator.SetInteger("direction", 4);
+                }
+                else
+                {
+                    animator.SetInteger("direction", 3);
+                }
+                break;
+            // Up & Right - 5
+            case (1, 1):
+                if (dir == "NW" || dir == "NE")
+                {
+                    animator.SetInteger("direction", 5);
+                }
+                else
+                {
+                    animator.SetInteger("direction", 6);
+                }
+                break;
+            // Up & Left - 6
+            case (-1, 1):
+                if (dir == "NW" || dir == "NE")
+                {
+                    animator.SetInteger("direction", 6);
+                }
+                else
+                {
+                    animator.SetInteger("direction", 5);
+                }
+                break;
+            // Down & Right - 7
+            case (1, -1):
+                if (dir == "NW" || dir == "NE")
+                {
+                    animator.SetInteger("direction", 7);
+                }
+                else
+                {
+                    animator.SetInteger("direction", 8);
+                }
+                break;
+            // Down & Left -- 8
+            case (-1, -1):
+                if (dir == "NW" || dir == "NE")
+                {
+                    animator.SetInteger("direction", 8);
+                }
+                else
+                {
+                    animator.SetInteger("direction", 7);
+                }
+                break;
+            default:
+                animator.SetInteger("direction", 0);
                 break;
         }
+
+        Debug.Log("rotation = " + transform.eulerAngles.y);
+
+        if (transform.eulerAngles.y <= 270 && transform.eulerAngles.y > 180)
+        {
+            dir = "SW";
+        }
+        else if (transform.eulerAngles.y <= 180 && transform.eulerAngles.y > 90)
+        {
+            dir = "SE";
+        }
+        else if (transform.eulerAngles.y <= 90 && transform.eulerAngles.y > 0)
+        {
+            dir = "NE";
+        }
+        else if (transform.eulerAngles.y <= 360 && transform.eulerAngles.y > 270)
+        {
+            dir = "NW";
+        }
+        else
+        {
+            dir = "None";
+        }
+
+        Debug.Log(dir);
     }
 
     private void Rotate()
@@ -71,6 +166,6 @@ public class PlayerMovement : MonoBehaviour
         Debug.Log(mousePosition);
         Vector2 direction = mousePosition - playerPosition;
         angle = new Vector3(direction.x, 0, direction.y);
-        transform.rotation = Quaternion.LookRotation(-angle);
+        transform.rotation = Quaternion.LookRotation(angle);
     }
 }
