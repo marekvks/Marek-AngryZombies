9eeb2c8c520e03e96681a40b294dcf9ea49b8d56
Marek <84468808+marekvks@users.noreply.github.com>
2022-01-23 00:27:03 +0100

    EnemyHealth Script, shoot interval
    player deals damage to the zombie and after 10 hits zombie dies. i also added shoot interval when player can shoot every 1.5 sec.

---
+++ Marek-AngryZombies/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float zombieHealth = 100f;
+
+    private void Update()
+    {
+        if (zombieHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
--- Marek-AngryZombies/Assets/Scripts/PlayerCombat.cs
+++ Marek-AngryZombies/Assets/Scripts/PlayerCombat.cs
@@ -7,35 +7,54 @@ public class PlayerCombat : MonoBehaviour
     public Transform gunEndpoint;
     public AudioSource gunAudioSource;
     public AudioClip m16GunSound;
-    private float shootInterval;
+    private float currentGunShootInterval = 0.15f;
+    private float shootInterval = 0f;
+    private float damage = 10f;
     public LayerMask layer;
     public ParticleSystem muzzleFlash;
     RaycastHit hit;
     private bool isPlayed = false;
 
+    /*       |
+     *      /|\
+     *     / | \
+     *    /  |  \
+     *       |
+     *       |
+     *       |
+     * 
+     * well-arranged code here ------> jk i will fix this but ill do it tomorrow
+     * 
+     * 
+     */
+
     private void Update()
     {
-        Debug.DrawRay(gunEndpoint.position, transform.forward, Color.red);
+        Debug.DrawRay(gunEndpoint.position, transform.forward, Color.red);  // Draw Ray because im stupid and i dont remember wich direction should it point :)
 
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKey(KeyCode.Mouse0) && Time.time > shootInterval)
         {
-            Shoot();
+            shootInterval = Time.time + currentGunShootInterval;    // time wtf
+            Shoot();    // bang bang
+            muzzleFlash.Stop();
         }
      }
 
     private void Shoot()
     {
-        gunAudioSource.PlayOneShot(m16GunSound);
+        gunAudioSource.PlayOneShot(m16GunSound);    // boom!
         if (!isPlayed)
         {
-            muzzleFlash.Play();
+            muzzleFlash.Play();     // Play muzzleFlash particle but ... it doesnt work yet :D
             isPlayed = true;
         }
-        if (Physics.Raycast(gunEndpoint.position, transform.forward, out hit, Mathf.Infinity, layer))
+        if (Physics.Raycast(gunEndpoint.position, transform.forward, out hit, Mathf.Infinity, layer))   // Ray Ray Ray Ray Ray Ray Ray Ray Ray Ray Ray Ray Ray Ray Ray Ray
         {
-            Debug.Log(hit.collider.gameObject.name);
-            hit.collider.gameObject.SetActive(false);
+            Debug.Log(hit.collider.gameObject.name);                            // Why debug.log? i dont know
+            hit.collider.GetComponent<EnemyHealth>().zombieHealth -= damage;    // Deals damage to the player lol
         }
+
+
         isPlayed = false;
     }
 }
